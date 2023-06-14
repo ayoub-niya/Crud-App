b18f93fda76c6d1b0f621c2d20769a3ccdfa9d83
A Y O U B <84485425+ayoub-niya@users.noreply.github.com>
2023-06-14 12:16:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 $host = "localhost";
-$dbname = "products";
+$dbname = "product_db";
 $username = "root";
 $password = "";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $query = "SELECT * FROM product_table";
+    $query = "SELECT * FROM products";
     $stmt = $pdo->prepare($query);
     $stmt->execute();
     $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -20,17 +20,18 @@ try {
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>My Crud App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>My Crud App</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <h1>Products Table</h1>
-    <a href="create.php" class="btn btn-success">Add product</a>
-    <table class="table">
+</head>
+<body>
+<div class="container p-4 w-75">
+  <h1 class="text-center mb-4">List Of Products</h1>
+  <a href="create.php" class="btn btn-success mb-2">Add product</a>
+  <table class="table">
   <thead>
     <tr>
       <th scope="col">ID</th>
@@ -38,27 +39,28 @@ try {
       <th scope="col">Description</th>
       <th scope="col">price</th>
       <th scope="col">Date created</th>
+      <th scope="col"></th>
     </tr>
   </thead>
   <tbody>
-    <?php foreach($products as $i => $product) : ?>
+  <?php foreach($products as $i => $product) : ?>
     <tr>
-      <th scope="row"><?php echo $i + 1; ?></th>
+    <th scope="row"><?php echo $i + 1; ?></th>
       <td><?php echo $product['title']; ?></td>
       <td><?php echo $product['description']; ?></td>
       <td><?php echo $product['price']; ?></td>
       <td><?php echo $product['create_date']; ?></td>
       <td>
       <a href="update.php?id=<?php echo $product['id']; ?>" class="btn btn-primary">Edit</a>
-        <form action="delete.php" method="post" style="display: inline-block;">
-            <input type="hidden" name="id" value="<?php echo $product['id']; ?>">
-            <button type="submit" class="btn btn-danger">Delete</button>
-        </form>
+      <form action="delete.php" method="post" style="display: inline-block;">
+      <input type="hidden" name="id" value="<?php echo $product['id']; ?>">
+      <button type="submit" class="btn btn-danger">Delete</button>
+      </form>
       </td>
     </tr>
-    <?php endforeach; ?>
+  <?php endforeach; ?>
   </tbody>
-</table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-  </body>
+  </table>
+</div>
+</body>
 </html>
